a678e4838fe5c43d78c474969386570fc5159f45
Joseph Herrera <josephherreraleon@gmail.com>
2023-03-07 07:48:25 -0600

    Version final de implementacion MVC

--- models/producto.js
+++ models/producto.js
@@ -1,22 +1,25 @@
 const fs = require("fs");
 const path = require("path");
+const ruta = path.join( 
+  path.dirname(process.mainModule.filename),
+  "datos",
+  "productos.json");
+
+const getProductosDesdArchivo = (cb) => {
+  fs.readFile(ruta, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
+};
 module.exports = class Producto {
   constructor(titulo) {
     this.titulo = titulo;
   }
   guardar() {
-    const ruta = path.join(
-      path.dirname(process.mainModule.filename),
-      "datos",
-      "productos.json"
-    );
-    // Leer archivos de esta manera, no es lo mas recomendable
-    // hay otra funcion createReadStream la cual funciona mejor
-    fs.readFile(ruta, (err, fileContent) => {
-      let productos = [];
-      if (!err) {
-        productos = JSON.parse(fileContent);
-      }
+    getProductosDesdArchivo(productos => {
       productos.push(this);
       fs.writeFile(ruta, JSON.stringify(productos), (err) => {
         console.log(err);
@@ -24,17 +27,6 @@ module.exports = class Producto {
     });
   }
   static mostrarTodo(cb) {
-    const ruta = path.join(
-      path.dirname(process.mainModule.filename),
-      "datos",
-      "productos.json"
-    );
-    fs.readFile(ruta, (err, fileContent) => {
-      if (err) {
-        cb([]);
-      } else {
-        cb(JSON.parse(fileContent));
-      }
-    });
+    getProductosDesdArchivo(cb);
   }
 };
