43216bd0d1650c65c5b68883cb7ba04f90fd7007
Joseph Herrera <josephherreraleon@gmail.com>
2023-03-08 13:25:42 -0600

    Cambio en la navegacion

--- controllers/producto.js
+++ controllers/producto.js
@@ -1,22 +1,25 @@
 const Producto = require("../models/producto");
+
 exports.getAgregarProducto = (req, res, next) => {
-  res.render("agregar-producto", {
-    tituloPagina: "Agregar Productos Nuevos",
+  res.render("admin/agregar-producto", {
+    tituloPagina: "Agregar Producto",
     path: "/admin/agregar-producto",
   });
 };
-exports.postAgregarProducto = (req, res, next) => {
-  const producto = new Producto(req.body.titulo);
-  producto.guardar();
-  res.redirect("/");
-};
+
 exports.getProductos = (req, res, next) => {
   Producto.mostrarTodo(productos => {
-    res.render("tienda", {
+    res.render("tienda/lista-productos", {
       prods: productos,
       tituloPagina: "Tienda",
       path: "/",
       tieneProductos: productos.length > 0,
     });
   });
+};
+
+exports.postAgregarProducto = (req, res, next) => {
+  const producto = new Producto(req.body.titulo);
+  producto.guardar();
+  res.redirect("/");
 };
