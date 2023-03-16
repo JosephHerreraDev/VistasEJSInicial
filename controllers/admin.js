20194de6a61e94dd38384ee46c9beb4ef322790f
Joseph Herrera <josephherreraleon@gmail.com>
2023-03-16 10:04:29 -0600

    Pre-llenado de la pagina editar producto con datos

--- controllers/admin.js
+++ controllers/admin.js
@@ -5,10 +5,25 @@ exports.getEditarProducto = (req, res, next) => {
   if (!modoEditar) {
     return res.redirect("/");
   }
+  const idProd = req.params.idProducto;
+  Producto.encontrarPorId(idProd, (producto) => {
+    if (!producto) {
+      return res.redirect("/");
+    }
+    res.render("admin/editar-producto", {
+      tituloPagina: "Editar Producto",
+      ruta: "/admin/editar-producto",
+      editando: modoEditar,
+      producto: producto,
+    });
+  });
+};
+
+exports.getAgregarProducto = (req, res, next) => {
   res.render("admin/editar-producto", {
-    tituloPagina: "Editar Producto",
-    ruta: "/admin/editar-producto",
-    editando: modoEditar,
+    tituloPagina: "Agregar Productos",
+    ruta: "/admin/agregar-producto",
+    editando: false,
   });
 };
 
@@ -20,6 +35,9 @@ exports.postAgregarProducto = (req, res, next) => {
   const producto = new Producto(titulo, urlImagen, descripcion, precio);
   producto.guardar();
   res.redirect("/");
+};
+
+exports.postEditarProducto = (req, res, next) => {
 };
 
 exports.getProductos = (req, res, next) => {
